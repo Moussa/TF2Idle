0b67de1ae14298553fc83384b211a60878f639cd
RJackson <rob@is.a-pirate.co.uk>
2011-12-15 12:47:28 +0000

    Removing my personal settings from config.

--- TF2IdleConfig.py
+++ TF2IdleConfig.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 config = {
-	'SteamLocation': r'G:/Steam', # Normal Steam installation location
-	'SecondarySteamapps': r'G:/Sandboxie/Steam/steamapps', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances.
+	'SteamLocation': r'C:/Program Files (x86)/Steam', # Normal Steam installation location
+	'SecondarySteamapps': r'', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances.
 	'SandboxieLocation': r'C:/Program Files/Sandboxie', # Sandboxie installation location
 	'Steam API Key': r'', # Steam API key
-	'BackpackViewer': r'TF2B', # Backpack viewer site (OPTF2, Steam, TF2B or TF2Items)
+	'BackpackViewer': r'', # Backpack viewer site (OPTF2, Steam, TF2B or TF2Items)
 	'IdleAccounts': [
 		{'username': r'MyAccount',
 		 'password': r'MyPassword',
