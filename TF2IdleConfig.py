eea1bb52281bbc4d839c0d7a9e7110370ef04a67
Moussekateer <moussa.uk@gmail.com>
2011-12-15 13:54:38 +0000

    Reverting changes to steaminstall, adding descriptions

--- TF2IdleConfig.py
+++ TF2IdleConfig.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 config = {
 	'SteamLocation': r'C:/Program Files (x86)/Steam', # Normal Steam installation location
-	'SecondarySteamapps': r'', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances.
+	'SecondarySteamapps': r'', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances. If not, set it to the steamapps folder of your secondary Steam installation.
 	'SandboxieLocation': r'C:/Program Files/Sandboxie', # Sandboxie installation location
 	'Steam API Key': r'', # Steam API key (If you wish to use the item logging function)
 	'BackpackViewer': r'', # Backpack viewer site. Options = OPTF2, Steam, TF2B, TF2Items
@@ -9,22 +9,21 @@ config = {
 		{'username': r'MyAccount',
 		 'password': r'MyPassword',
 		 'steamID': r'MySteamID',
-		 'sandboxname': r'MySandboxName',
-		 'steaminstall': r'G:/Sandboxie/Steam/1',
-		 'premium': False
+		 'sandboxname': r'MySandboxName', # Sandbox name in Sandboxie
+		 'steaminstall': r'G:/Sandboxie/Steam/1' # Directory that contains Steam.exe
 		},
 		{'username': r'MySecondAccount',
 		 'password': r'MyPassword',
 		 'steamID': r'MySteamID',
 		 'sandboxname': r'MySandboxName',
 		 'steaminstall': r'G:/Sandboxie/Steam/2',
-		 'premium': True
+		 'f2p': True
 		},
 		{'username': r'MyThirdAccount',
 		 'password': r'MyPassword',
 		 'steamID': r'MySteamID',
 		 'sandboxname': r'MySandboxName',
-		 'premium': True
+		 'steaminstall': r'G:/Sandboxie/Steam/3'
 		}
 	]
 }
