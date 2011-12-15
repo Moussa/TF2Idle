0cb5c5e083b1e41ee1c8b269d07424e417826011
RJackson <rob@is.a-pirate.co.uk>
2011-12-15 12:59:06 +0000

    Explaining and clarifying some settings in the config.

--- TF2IdleConfig.py
+++ TF2IdleConfig.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 config = {
 	'SteamLocation': r'C:/Program Files (x86)/Steam', # Normal Steam installation location
-	'SecondarySteamapps': r'', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances.
+	'SecondarySteamapps': r'', # Secondary Steamapps folder that's shared between multiple instances of Steam, if you're using multiple instances.  If not, set it to the steamapps folder of your secondary Steam installation.
 	'SandboxieLocation': r'C:/Program Files/Sandboxie', # Sandboxie installation location
 	'Steam API Key': r'', # Steam API key
 	'BackpackViewer': r'', # Backpack viewer site (OPTF2, Steam, TF2B or TF2Items)
@@ -10,15 +10,15 @@ config = {
 		 'password': r'MyPassword',
 		 'steamID': r'MySteamID',
 		 'sandboxname': r'MySandboxName',
-		 'steaminstall': r'G:/Sandboxie/Steam/1',
-		 'premium': False
+		 'steaminstall': r'', # Which steam install to use for this account.  If you're launching all accounts from a single Steam install, set this to the same value for all accounts.  E.g. C:\Program Files (x86)\SecondarySteam\
+		 'premium': False # Whether this particular account owns a premium copy of TF2.  F2P = False, Premium = True.
 		},
 		{'username': r'MySecondAccount',
 		 'password': r'MyPassword',
 		 'steamID': r'MySteamID',
 		 'sandboxname': r'MySandboxName',
-		 'steaminstall': r'G:/Sandboxie/Steam/2',
-		 'premium': True
+		 'steaminstall': r'', # Which steam install to use for this account.  If you're launching all accounts from a single Steam install, set this to the same value for all accounts.  E.g. C:\Program Files (x86)\SecondarySteam\
+		 'premium': True # Whether this particular account owns a premium copy of TF2.  F2P = False, Premium = True.
 		}
 	]
 }
