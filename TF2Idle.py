9bd90d1462b62af62f61fdf6a69f57570b1c5549
RJackson <rob@is.a-pirate.co.uk>
2011-12-16 10:07:52 +0000

    Fixed the item logger not logging drops, bcuz tf2.py is sucky and makes me cry at night while hugging many kittens.

--- TF2Idle.py
+++ TF2Idle.py
@@ -178,7 +178,7 @@ def startLog(screen):
 				# Check to see if item with highest ID has changed
 				if newestitem['id'] != lastIDlist[n]:
 					lastIDlist[n] = newestitem['id']
-					if newestitem['item_class'] != 'supply_crate':
+					if newestitem['item_name'] not in ['Mann Co. Supply Crate', 'Festive Winter Crate', 'Refreshing Summer Cooler', 'Salvaged Mann Co. Supply Crate', 'Naughty Winter Crate', 'Nice Winter Crate']:
 						currenttimestr = time.strftime('%H:%M', time.localtime(time.time()))
 						output = {'item': newestitem['item_name'].encode('utf8'), 'item_slot': newestitem['item_slot'], 'account': account, 'time': currenttimestr}
 						finds.pop(0)
